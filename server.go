0a148b216d84c41cfe4cecc7911fb99f5d6f84ec
ammarm08 <ammar.mian08@gmail.com>
2015-12-11 23:19:24 -0500

    a tcp commandline based chat app

--- server.go
+++ server.go
@@ -2,20 +2,45 @@ package main
 
 import (
     "fmt"
+    "io"
     "log"
-    "net/http"
+    "net"
 )
 
-const PORT = "4000"
+const PORT = "localhost:4000"
 
 func main() {
-    http.HandleFunc("/", handler)
-    err := http.ListenAndServe("localhost:"+PORT, nil)
+    server, err := net.Listen("tcp", PORT)
     if err != nil {
+      log.Fatal(err)
+    }
+    for {
+      conn, err := server.Accept()
+      if err != nil {
         log.Fatal(err)
+      }
+      go match(conn)
     }
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Alohamora")
+var partner = make(chan io.ReadWriteCloser)
+
+func match(conn io.ReadWriteCloser) {
+  fmt.Fprint(conn, "Waiting for someone to join chat...")
+  select {
+  case partner <- conn:
+    // data sent from input conn to the "partner" channel
+  case p := <- partner:
+    // data sent from existing partner to newly initialized p
+    // which implicity gets a type io.ReadWriteCloser as well
+    // looks like we have a chat on our hands now!
+    chat(p, conn)
+  }
+}
+
+func chat(a, b io.ReadWriteCloser) {
+  fmt.Fprintln(a, "Gotchu a friend!")
+  fmt.Fprintln(b, "Gotchu a friend!")
+  go io.Copy(a, b)
+  io.Copy(b, a)
 }
